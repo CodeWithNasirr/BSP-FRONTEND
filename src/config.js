38acb9d341397d75cc0dff1fda97a185c012daf9
CodeWithNasirr <skofficial665@gmail.com>
2025-03-11 12:32:59 +0530

    small Changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = "https://whatsappx.up.railway.app"; // Replace with your Elastic IP
-const API_BASE_URL = "http://127.0.0.1:8000"; // Replace with your Elastic IP
+const API_BASE_URL = "https://whatsappx.up.railway.app"; // Replace with your Elastic IP
+// const API_BASE_URL = "http://127.0.0.1:8000"; // Replace with your Elastic IP
 
 export default API_BASE_URL;
