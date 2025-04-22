c849e01b6ec9194af0698ceacadea5a25ff6e6ec
Nasir <skofficial665@gmail.com>
2025-04-22 10:50:24 +0530

    bug fix

--- src/components/Contact/BulkImportContacts.jsx
+++ src/components/Contact/BulkImportContacts.jsx
@@ -27,7 +27,7 @@ const BulkImportContacts = () => {
                 'Content-Type': 'multipart/form-data',
               },
             });
-      toast.success("Contacts imported successfully!");
+      toast.success(response.data.message);
       console.log(response.data);
     } catch (error) {
       toast.error("Error importing contacts.");
@@ -36,7 +36,7 @@ const BulkImportContacts = () => {
       setIsUploading(false);
     }
   };
-  
+
   return (
     <div className="max-w-md mx-auto  bg-white p-6 rounded-2xl shadow-xl">
       <h2 className="text-xl font-semibold mb-4 text-center">📥 Bulk Import Contacts</h2>
