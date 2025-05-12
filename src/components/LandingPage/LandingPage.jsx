44d28cec0d6f968ffdca7b1f9bfc8ac44e70d021
Nasir <skofficial665@gmail.com>
2025-05-12 21:01:46 +0530

    bug fix

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -167,7 +167,7 @@ export default function LandingPage() {
                 </svg>
                 Login
               </a>
-              <a className="text-sm xl:text-base text-white px-3 md:px-5 py-1.5 md:py-2.5 rounded-full bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-700 transition-all duration-300 flex items-center shadow-md" href="/signup">
+              <a className="text-sm xl:text-base text-white px-3 md:px-5 py-1.5 md:py-2.5 rounded-full bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-700 transition-all duration-300 flex items-center shadow-md" href="/register">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-3.5 md:w-4 h-3.5 md:h-4 mr-0.5">
                   <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"></path>
                   <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"></path>
