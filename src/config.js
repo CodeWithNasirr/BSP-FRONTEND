5dfe7ccd2c38606494c2164a5a81a182c2928530
Sk Nasir <skofficial665@gmail.com>
2025-12-16 17:44:19 +0530

    bug issued

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // const API_BASE_URL = "https://whatsappx.up.railway.app"; 
 // const API_BASE_URL = "https://indianchiplevelcommunity.com";
 const API_BASE_URL = "https://admin.numlockitsolutions.co.in";
-
-// const API_BASE_URL = "https://a8bddd425bd0.ngrok-free.app";  
+ 
+// const API_BASE_URL = "https://a8bddd425bd0.ngrok-free.app"
 // const API_BASE_URL = "http://127.0.0.1:8000";   
 //  
 export default API_BASE_URL;
