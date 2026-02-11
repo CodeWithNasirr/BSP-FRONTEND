e2adee76c2708f4cf91a992869f60c008f2bfa07
Sk Nasir <skofficial665@gmail.com>
2026-02-11 23:18:58 +0530

    chat system new update jan11

--- src/components/Chat/MarkPurchaseModal.jsx
+++ src/components/Chat/MarkPurchaseModal.jsx
@@ -57,7 +57,7 @@ const MarkPurchaseModal = ({
 
       toast.success('Purchase marked successfully');
       onClose();
-      // fetchChatList();
+      fetchChatList();
     } catch (error) {
       console.log(error)
       toast.error(error.response?.data?.error || 'Failed to mark purchase');
