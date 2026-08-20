5b2b0c0b8be610020202b4528485d098b07bba5d
Sk Nasir <skofficial665@gmail.com>
2026-08-20 08:42:01 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-const API_BASE_URL = "https://api.gptxplatform.com";
-// const API_BASE_URL = "https://admin.numlockitsolutions.co.in";
+// const API_BASE_URL = "https://api.gptxplatform.com";
+const API_BASE_URL = "https://bsp-backendxx-production.up.railway.app";
 // const API_BASE_URL = "http://127.0.0.1:8000";
 
 export default API_BASE_URL;
